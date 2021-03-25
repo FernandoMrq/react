7f7b3918cbc638a2354569711e4f6335065d6ddf
unknown <frndoliveira@gmail.com>
2021-03-25 08:14:56 -0300

    25/03/2021

--- src/App.js
+++ src/App.js
@@ -1,92 +1,60 @@
 import logo from './logo.svg';
 import { Component } from 'react';
+import './App.css'
 
 class App extends Component {
 
     state = {
-      posts: [
-        {
-          id: 1,
-          title: 'O título 1',
-          body: 'O corpo 1'
-        },
-        {
-          id: 2,
-          title: 'O título 2',
-          body: 'O corpo 2'
-        },
-        {
-          id: 3,
-          title: 'O título 3',
-          body: 'O corpo 3'
-        }
-      ],
-      counter: 0
+      posts: [],
     };
 
-    timeOutUpdate= null
-
-  handleTimeout = () => {
-    const {posts, counter} = this.state;
-    posts[0].title = 'O título mudou';
-    this.timeOutUpdate = setTimeout(() => {
-    // setTimeout(() => {
-      this.setState({ posts, counter: counter + 1})
-    }, 1000);
+  componentDidMount() {
+    this.loadPosts();
   }
 
-  componentDidMount() {
-    // console.log('OI');
-    this.handleTimeout();
+  loadPosts = async () => {
+    const postResponse    = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse  = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([ postResponse, photosResponse ]);
+
+    const postJson        = await posts.json();
+    const photosJson      = await photos.json();
+
+    const postsAndPhotos = postJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos });
   }
 
   componentDidUpdate(){
-    this.handleTimeout();
   }
 
   componentWillUnmount(){
-    clearTimeout(this.timeOutUpdate);
   }
 
   render() {
-
-      const { posts, counter } = this.state;
+      const { posts } = this.state;
 
       return (
-      <div className="App">
-        <h1>{ counter }</h1>
-        {/* <h1>{ counter }</h1> */}
-        {posts.map(post => (
-          <div key={post.id}>
-            <h1> { post.title } </h1>
-            <p>
-              { post.body }
-            </p>
+        <section className="container">
+          <div className="App" className="posts">
+            {posts.map(post => (
+              <div className="post">
+                <img src={ post.cover } alt={post.title}/>
+                <div key={post.id} className="post-content">
+                  <h1> { post.title } </h1>
+                  <p>
+                    { post.body }
+                  </p>
+                </div>
+              </div>
+            ))}
           </div>
-      ))}
-      </div>
+        </section>
     );
   }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
